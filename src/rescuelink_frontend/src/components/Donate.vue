254d8d4967d1b7a32f466733d1c6eded8e74bbf0
jaysonalfie <jaysonalfie@gmail.com>
2024-09-05 10:19:53 +0300

    changes to frontend components

--- src/rescuelink_frontend/src/components/Donate.vue
+++ src/rescuelink_frontend/src/components/Donate.vue
@@ -1,50 +1,93 @@
+ <script setup>
+  
+  // export default {
+  //   data() {
+  //     return {
+  //       donations: [],
+  //       newDonation: {
+  //         name: '',
+  //         amount: 0
+  //       }
+  //     };
+  //   },
+  //   methods: {
+  //     async donate() {
+  //       console.log('Button is clicked');
+  //       await this.$root.callBackend('donate', this.newDonation.name, parseInt(this.newDonation.amount));
+  //       this.newDonation.name = '';
+  //       this.newDonation.amount = 0;
+  //       this.getDonations();
+  //     },
+  //     async getDonations() {
+  //       const donations = await this.$root.callBackend('getDonations');
+  //       this.donations = donations;
+  //     }
+  //   },
+  //   mounted() {
+  //     this.getDonations();
+  //   }
+  // };
+  import { ref } from 'vue';
+  import {rescuelink_backend} from 'declarations/rescuelink_backend/index';
+   
+  //variables to store donations and response
+  let donationsSubmitted = ref([]);
+  let donationResponse = ref('');
+
+  async function handleDonationSubmit(e) {
+    e.preventDefault();
+    const target = e.target;
+    const donorNameInput = target.querySelector('#donorName');
+    const donationAmountInput = target.querySelector('#donationAmount');
+
+    const name = donorNameInput.value;
+    const amount = parseInt(donationAmountInput.value);//converting string to int
+
+
+    if(name && amount){
+      try{
+        //calling backend function to addDonation 
+        const response = await rescuelink_backend.addDonation(name,amount);
+
+        //Push the new donation into the donations array
+        donationsSubmitted.value.push([name,amount]);
+
+        //Store the backend response in the reactive variable
+        donationResponse.value =response;
+
+        //Clearing input fields
+        donorNameInput.value = '';
+        donationAmountInput.value='';
+
+
+      } catch(error){
+        alert('There was an issue with your donation. Please try again')
+      }
+    } else {
+      alert('Please enter both name and donation amount');
+    }
+    
+  }
+  </script>
 
 <template>
-    <section>
-      <h1>ReliefLink - Decentralized Disaster Relief Coordination</h1>
-      <h2>Donate</h2>
-     <div class="donate_content">
-      <input v-model="newDonation.name" placeholder="Your Name">
-      <input v-model="newDonation.amount" type="number" placeholder="Amount">
-      <button class="button" @click="donate">Donate</button>
-      <ul>
-        <li v-for="donation in donations" :key="donation[0]">{{ donation[0] }}: {{ donation[1] }} ICP</li>
-      </ul>
-      </div>
-    </section>
-    
-  </template>
+  <section>
+    <h1>ReliefLink - Decentralized Disaster Relief Coordination</h1>
+    <h2>Donate</h2>
+   <div class="donate_content">
+   <form action="#" @submit="handleDonationSubmit">
+    <input id="donorName"placeholder="Your Name">
+    <input id="donationAmount" type="number" placeholder="Amount">
+    <button class="button" type="submit">Donate</button>
+   </form>
+    <!-- <ul>
+      <li v-for="donation in donations" :key="donation[0]">{{ donation[0] }}: {{ donation[1] }} ICP</li>
+    </ul> -->
+    <p id="donationResponse"v-if="donationResponse">{{ donationResponse }}</p>
+    </div>
+  </section>
   
-  <script>
-  
-  export default {
-    data() {
-      return {
-        donations: [],
-        newDonation: {
-          name: '',
-          amount: 0
-        }
-      };
-    },
-    methods: {
-      async donate() {
-        console.log('Button is clicked');
-        await this.$root.callBackend('donate', this.newDonation.name, parseInt(this.newDonation.amount));
-        this.newDonation.name = '';
-        this.newDonation.amount = 0;
-        this.getDonations();
-      },
-      async getDonations() {
-        const donations = await this.$root.callBackend('getDonations');
-        this.donations = donations;
-      }
-    },
-    mounted() {
-      this.getDonations();
-    }
-  };
-  </script>
+</template>
   
   <style >
   @import './style.css';
--- src/rescuelink_frontend/src/components/Resources.vue
+++ src/rescuelink_frontend/src/components/Resources.vue
@@ -1,39 +1,61 @@
 <template>
     <section>
+     <form action="#" @submit="handleResourceSubmit">
       <h2>Resources</h2>
-      <input v-model="newResource.resource" placeholder="Resource Description">
-      <button class="button" @click="addResource">Add Resource</button>
-      <ul>
+      <input id="resource" placeholder="Resource Description">
+      <button class="button" type="submit">Add Resource</button>
+      <!-- <ul>
         <li v-for="resource in resources" :key="resource">{{ resource }}</li>
-      </ul>
+      </ul> -->
+    </form>
+
+    <section id="resourceSubmitted">{{ resourceSubmitted }}</section>
     </section>
   </template>
   
-  <script>
-  export default {
-    data() {
-      return {
-        resources: [],
-        newResource: {
-          resource: ''
-        }
-      };
-    },
-    methods: {
-      async addResource() {
-        await this.$root.callBackend('addResource', this.newResource.resource);
-        this.newResource.resource = '';
-        this.getResources();
-      },
-      async getResources() {
-        const resources = await this.$root.callBackend('getResources');
-        this.resources = resources;
-      }
-    },
-    mounted() {
-      this.getResources();
-    }
-  };
+  <script setup>
+  // export default {
+  //   data() {
+  //     return {
+  //       resources: [],
+  //       newResource: {
+  //         resource: ''
+  //       }
+  //     };
+  //   },
+  //   methods: {
+  //     async addResource() {
+  //       await this.$root.callBackend('addResource', this.newResource.resource);
+  //       this.newResource.resource = '';
+  //       this.getResources();
+  //     },
+  //     async getResources() {
+  //       const resources = await this.$root.callBackend('getResources');
+  //       this.resources = resources;
+  //     }
+  //   },
+  //   mounted() {
+  //     this.getResources();
+  //   }
+  // };
+  import { ref } from 'vue';
+  import {rescuelink_backend} from 'declarations/rescuelink_backend/index';
+
+  //ref allows to create a reactive state
+  let resourceSubmitted = ref('');
+
+  async function handleResourceSubmit(e){
+    e.preventDefault();
+    const target = e.target;
+    const resourceInput = target.querySelector('#resource');
+    const resource = resourceInput.value;
+    await rescuelink_backend.enterResource(resource).then((response) => {
+      resourceSubmitted.value = response;
+
+      //clear the input field after successful submission
+      resourceInput.value='';
+    });
+  }
   </script>
   
   <style scoped>
